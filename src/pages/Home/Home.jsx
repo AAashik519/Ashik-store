8381e9c83bc7ce53e3f622da1ffab33fb951d28a
AAashik519 <64531121+AAashik519@users.noreply.github.com>
2022-12-02 04:24:08 +0600

    category

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import './Home.scss'
 import Slider from '../../components/Slider/Slider'
 import FeaturedProducts from '../../components/Featured/FeaturedProducts'
+import Categories from '../../components/Categories/Categories'
 
 const Home = () => {
   return (
     <div> 
       <Slider />
       <FeaturedProducts type="Featured" />
-      {/* <FeaturedProducts type="Trending" /> */}
+      <Categories />
+      <FeaturedProducts type="Trending" />
     </div>
   )
 }
